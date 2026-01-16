81f8219fa115da3376d3c08ff646752386f28e68
Baldo Campos <baldomanage@gmail.com>
2026-01-16 08:49:29 -0800

    feat: :sparkles: Layout Component and children routes with layout component

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
+import { CountryLayout } from './layouts/CountryLayout/CountryLayout';
 import { ByCapitalPage } from './pages/by-capital-page/by-capital-page';
 
 export const countryRoutes: Routes = [
   {
     path: '',
-    component: ByCapitalPage,
+    component: CountryLayout,
+    children: [
+      { path: 'by-capital', component: ByCapitalPage },
+      { path: '**', redirectTo: 'by-capital' },
+    ],
   },
 ];
 
