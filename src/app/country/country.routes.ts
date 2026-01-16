b4851321883e8d38c788d513e6eaf61ee6906af0
Baldo Campos <baldomanage@gmail.com>
2026-01-16 09:46:46 -0800

    feat: :sparkles: Added components ByCountry and ByRegion

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CountryLayout } from './layouts/CountryLayout/CountryLayout';
 import { ByCapitalPage } from './pages/by-capital-page/by-capital-page';
+import { ByCountryPage } from './pages/by-country-page/by-country-page';
+import { ByRegionPage } from './pages/by-region-page/by-region-page';
 
 export const countryRoutes: Routes = [
   {
@@ -8,6 +10,8 @@ export const countryRoutes: Routes = [
     component: CountryLayout,
     children: [
       { path: 'by-capital', component: ByCapitalPage },
+      { path: 'by-country', component: ByCountryPage },
+      { path: 'by-region', component: ByRegionPage },
       { path: '**', redirectTo: 'by-capital' },
     ],
   },
