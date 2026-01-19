1a7821964181f0f0bc39c1b8121b23030a843939
Baldo Campos <baldomanage@gmail.com>
2026-01-19 11:49:33 -0800

    feat: :sparkles: Created interface and mapper for restCountry to Country

---
+++ src/app/country/mappers/country.mapper.ts
@@ -0,0 +1,19 @@
+import { Country } from '../interfaces/country.interface';
+
+export class CountryMapper {
+  static RestCountryToCountry(restCountry: RESTCountry): Country {
+    return {
+      cca2: restCountry.cca2,
+      flag: restCountry.flag,
+      FlagSvg: restCountry.flags.svg,
+      name: restCountry.name.official,
+      capital: restCountry.capital[0],
+      population: restCountry.population,
+    };
+  }
+
+  static RestCountriesToCountries(restCountries: RESTCountry[]): Country[] {
+    // return restCountries.map(this.RestCountryToCountry); // Funciona de la misma manera
+    return restCountries.map((restCountry) => this.RestCountryToCountry(restCountry));
+  }
+}
