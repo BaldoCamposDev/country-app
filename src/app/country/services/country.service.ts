442ded1a05655b20ce0ca515149ae98fb8fea51f
Baldo Campos <baldomanage@gmail.com>
2026-01-21 14:12:54 -0800

    feat: :sparkles: Added searchByRegion functionality

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -3,6 +3,7 @@ import { inject, Injectable } from '@angular/core';
 import { catchError, delay, map, of, tap, throwError } from 'rxjs';
 import { CountryMapper } from '../mappers/country.mapper';
 import { Country } from '../interfaces/country.interface';
+import { Region } from '../interfaces/region.type';
 
 const API_URL = 'https://restcountries.com/v3.1';
 
@@ -15,6 +16,8 @@ export class CountryService {
   private queryCacheCapital = new Map<string, Country[]>();
 
   private queryCacheCountry = new Map<string, Country[]>();
+
+  private queryCacheRegion = new Map<Region, Country[]>();
 
   searchByCapital(query: string) {
     query = query.trim().toLowerCase();
@@ -51,6 +54,22 @@ export class CountryService {
         console.log(err);
 
         return throwError(() => new Error(`No se pudo obtener paises con ese query ${query}`));
+      }),
+    );
+  }
+
+  searchByRegion(region: Region) {
+    if (this.queryCacheRegion.has(region)) {
+      return of(this.queryCacheRegion.get(region) ?? []);
+    }
+
+    console.log(`Llegando al servidor con region ${region}`);
+
+    return this.http.get<RESTCountry[]>(`${API_URL}/region/${region}`).pipe(
+      map((countries) => CountryMapper.RestCountriesToCountries(countries)),
+      tap((countries) => this.queryCacheRegion.set(region, countries)),
+      catchError((err) => {
+        return throwError(() => err);
       }),
     );
   }
