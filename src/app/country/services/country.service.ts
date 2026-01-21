ce9717661a959cbd2d3e36bf9cbc9244d60afb0a
Baldo Campos <baldomanage@gmail.com>
2026-01-21 13:46:35 -0800

    feat: :sparkles: Added cache functionality

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { catchError, delay, map, of, throwError } from 'rxjs';
+import { catchError, delay, map, of, tap, throwError } from 'rxjs';
 import { CountryMapper } from '../mappers/country.mapper';
+import { Country } from '../interfaces/country.interface';
 
 const API_URL = 'https://restcountries.com/v3.1';
 
@@ -11,11 +12,22 @@ const API_URL = 'https://restcountries.com/v3.1';
 export class CountryService {
   private http = inject(HttpClient);
 
+  private queryCacheCapital = new Map<string, Country[]>();
+
+  private queryCacheCountry = new Map<string, Country[]>();
+
   searchByCapital(query: string) {
     query = query.trim().toLowerCase();
 
+    if (this.queryCacheCapital.has(query)) {
+      return of(this.queryCacheCapital.get(query));
+    }
+
+    console.log(`llegando al servidor por ${query}`);
+
     return this.http.get<RESTCountry[]>(`${API_URL}/capital/${query}`).pipe(
       map((restCountries) => CountryMapper.RestCountriesToCountries(restCountries)),
+      tap((countries) => this.queryCacheCapital.set(query, countries)),
       catchError((error) => {
         return throwError(() => new Error(`No se pudo obtener paises con ese query ${query}`));
       }),
@@ -25,9 +37,16 @@ export class CountryService {
   searchByCountry(query: string) {
     query = query.trim().toLowerCase();
 
+    if (this.queryCacheCountry.has(query)) {
+      return of(this.queryCacheCountry.get(query));
+    }
+
+    console.log(`llegando al servidor por ${query}`);
+
     return this.http.get<RESTCountry[]>(`${API_URL}/name/${query}`).pipe(
       map((restCountries) => CountryMapper.RestCountriesToCountries(restCountries)),
-      delay(3000),
+      tap((countries) => this.queryCacheCountry.set(query, countries)),
+      delay(2000),
       catchError((err) => {
         console.log(err);
 
